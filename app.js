a0096c4fcf5d58790b345c1c86f1270f3d19b49d
abdul azeem <kapriazeem@gmail.com>
2020-07-26 19:22:11 +0500

    Successful todo app

---
+++ app.js
@@ -0,0 +1,48 @@
+var list =document.getElementById("list");
+
+function addTodo(){
+    var todo_item =document.getElementById("todo-item");
+    //create li tag with text node
+    var li =document.createElement('li')
+    var addli =document.createTextNode(todo_item.value)
+    li.appendChild(addli)
+
+    //create delete button
+    var delBtn =document.createElement("button")
+    var delText =document.createTextNode("Delete") 
+    delBtn.setAttribute("class","btn")
+    delBtn.setAttribute("onclick","deleteItem(this)")
+    delBtn.appendChild(delText)
+    
+    //edit button
+
+    var editBtn =document.createElement("button")
+    var editText =document.createTextNode("EDIT")
+    editBtn.setAttribute("class","ebtn")
+    editBtn.appendChild(editText)
+    editBtn.setAttribute("onclick","editItem(this)")
+
+    li.appendChild(delBtn)
+
+    li.appendChild(editBtn)
+
+    list.appendChild(li)
+    todo_item.value=""
+    
+} 
+
+function deleteItem(e){
+      
+    e.parentNode.remove()
+}
+
+function editItem(e){
+    var val =e.parentNode.firstChild.nodeValue;
+    var editValue =prompt("Enter edit value",val)
+    e.parentNode.firstChild.nodeValue =editValue
+}
+
+
+function delAll(){
+    list.innerHTML=""
+}
